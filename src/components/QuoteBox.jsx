e849782951e40cc656adac8d7041257cd1f3a9e8
Moustapha <amroune.moustapha@gmail.com>
2025-07-21 15:04:02 +0200

    jsx layout for quote-box (without functional logic)

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const QuoteBox = () => {
+  return (
+    <div>
+      
+    <div id="quote-box">
+        <span id="text"></span>
+        <span id="author"></span>
+        <div>
+            <a href="twitter.com"></a>
+            <a href=""></a>
+            <button>New quote</button>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default QuoteBox
